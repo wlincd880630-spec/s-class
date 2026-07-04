3c687bfbd1be4a55863e056fa1125c24db919d44
wlincd880630 <wlincd880630@gmail.com>
2026-07-05 07:57:03 +0800

    Expand word image batch progress JSON for grade 5GA with new vocabulary entries and translations. Update data.js to link to new image resources for improved educational content and user experience.

--- Primary/School_textbook/Courseware/5GA/assets/data/data.js
+++ Primary/School_textbook/Courseware/5GA/assets/data/data.js
@@ -235,7 +235,7 @@ const TEXTBOOK_DATA = {
               "letter": "s"
             }
           ],
-          "image": "https://s-class-1403296481.cos.ap-chengdu.myqcloud.com/s-class/Primary/School_textbook/Courseware/assets/images/sentences/us.jpg",
+          "image": "https://s-class-1403296481.cos.ap-chengdu.myqcloud.com/s-class/Primary/School_textbook/Courseware/5GA/assets/images/words/Unit1/us/us_%E9%A3%8E%E6%A0%BCA.png",
           "sentences": [
             {
               "en": "The teacher told us a story.",
